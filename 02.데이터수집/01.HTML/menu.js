23f43c8d94de1f69675a3998b431144fa719b163
wawdse <yadnah@naver.com>
2025-09-03 19:53:39 +0900

    Add gmarket.js

---
+++ 02.데이터수집/01.HTML/menu.js
@@ -0,0 +1,82 @@
+data=[
+	{
+		"title": "유자리카노",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202508180839306070.png"
+	},
+	{
+		"title": "아샷추",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202507250814260600.png"
+	},
+	{
+		"title": "몬스터 아메리카노",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220339221050.png"
+	},
+	{
+		"title": "블랙아리아아메리카노",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202308310540309360.png"
+	},
+	{
+		"title": "블랙아리아딥라떼",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202308310540430220.png"
+	},
+	{
+		"title": "돌체 라떼",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220340359170.png"
+	},
+	{
+		"title": "디카페인아메리카노",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220340584680.png"
+	},
+	{
+		"title": "디카페인카페 라떼",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220341154100.png"
+	},
+	{
+		"title": "디카페인바닐라 딜라이트",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220349010700.png"
+	},
+	{
+		"title": "콜드브루 딜라이트",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220349272270.png"
+	},
+	{
+		"title": "콜드브루 라떼",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220351341860.png"
+	},
+	{
+		"title": "콜드브루",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220351480160.png"
+	},
+	{
+		"title": "바닐라 딜라이트",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220352085560.png"
+	},
+	{
+		"title": "카라멜 마키아또",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220352241540.png"
+	},
+	{
+		"title": "카페 모카",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220352386460.png"
+	},
+	{
+		"title": "카푸치노",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220352492730.png"
+	},
+	{
+		"title": "카페 라떼",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220353010640.png"
+	},
+	{
+		"title": "아메리카노",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220353165110.png"
+	},
+	{
+		"title": "아포가토",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220353281780.png"
+	},
+	{
+		"title": "에스프레소",
+		"url": "https://admin.hollys.co.kr/upload/menu/etc/menuEtc_202504220353379090.png"
+	}
+]
