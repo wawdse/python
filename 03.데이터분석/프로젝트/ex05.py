df1f2e4626864ead340e8e0a230fa65266736c72
wawdse <yadnah@naver.com>
2025-09-12 18:15:18 +0900

    09-12

--- 03.데이터분석/프로젝트/ex05.py
+++ 03.데이터분석/프로젝트/ex05.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 
-file_score ='data/학생성적.csv'
-file_info ='data/학생정보.csv'
+file_score ='c:/python/03.데이터분석/data/학생성적.csv'
+file_info ='c:/python/03.데이터분석/data/학생정보.csv'
 
 def inputNum(title):
     while True:
@@ -19,8 +19,9 @@ def submenu():
         score = pd.read_csv(file_score, index_col='지원번호')
         info = pd.read_csv(file_info, index_col='지원번호')
         df = info.join(score)
-        df['총점'] = df.apply(lambda x:sum(x['국어':'사회']), axis=1)
+        df['총점'] = df.apply(lambda x: sum(x['국어':'사회']), axis=1)
         df['평균'] = df['총점']/5
+
         df.fillna(0, inplace=True)
         cols = ['국어', '영어', '수학', '사회', '과학']
 
@@ -54,22 +55,21 @@ def submenu():
         elif menu=='2': #목록
             while True:
                 sel = inputNum('1.최신입력순|2.이름순|3.성적순>')
-                if sel=='':break
+                if sel=='': break
                 elif sel==1: df=df.sort_index(ascending=False)
                 elif sel==2: df=df.sort_values('이름')
-                elif sel==3: df.sort_values('평균', ascending=False)
-                for idx in df.head(3).head2index:
+                elif sel==3: df=df.sort_values('평균', ascending=False)
+                for idx in df.head(3).index:
                     row = df.loc[idx]
-                    print(f'지원번호:{idx}', end=' ')
+                    print(f'지원번호:{idx:02d}', end=' ')
                     print(f'이름:{row["이름"]}', end=' ')
                     print(f'학교:{row["학교"]}')
                     for col in cols:
                         print(f'{col}:{row[col]:.0f}', end=' ')
                     print(f'평균:{row["평균"]:.2f}')
-                    print()
                     print('-' * 60)
             input('아무키나 누르세요!')
-        elif menu=='3': #검색
+        elif menu=='3':#검색
             while True:
                 sel = inputNum('1.지원번호|2.학교|3.이름>')
                 if sel=='':
@@ -96,11 +96,12 @@ def submenu():
                     for idx in df[filt].index:
                         row = df.loc[idx]
                         print(f'지원번호:{idx} 이름:{row["이름"]} 학교:{row["학교"]} 평균:{row["평균"]:.2f}')
+
             input('아무키나 누르세요!')
-        elif menu=='4': #삭제
+        elif menu=='4':#삭제
             no = inputNum('지원번호>')
             if not no in info.index:
-                print('등록된 지원번호가 아닙니다')
+                print('등록된 지원번호가 아닙니다.')
             elif not no in score.index:
                 print('등록된 성적이 없습니다.')
             else:
@@ -108,7 +109,7 @@ def submenu():
                 print(f'이름:{row["이름"]}')
                 for col in cols:
                     print(f'{col}:{row[col]}')
-                sel=input('정말로 삭제하실래요(Y)')
+                sel=input('정말로 삭제하실래요(Y)>')
                 if sel=='Y' or sel=='y':
                     score.drop(index=no, inplace=True)
                     score.to_csv(file_score)
@@ -133,6 +134,7 @@ def submenu():
                     score.loc[no] = grade
                     score.to_csv(file_score)
                     print('수정완료!')
+
             input('아무키나 누르세요!')
         else:
             input('0~5번 숫자를 입력하세요!')
